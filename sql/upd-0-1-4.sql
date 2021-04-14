4958ce7d123b314aca8bcb1f250b25a8e672dad0
petak23 <petak23@gmail.com>
2021-04-14 14:51:11 +0200

    01 - Cistenie

--- sql/upd-0-1-4.sql
+++
@@ -1,5 +0,0 @@
-UPDATE `oblast_prvky` SET `c2` = '1158' WHERE `id` = '347';
-UPDATE `oblast_prvky` SET `c2` = '658' WHERE `id` = '82';
-UPDATE `oblast_prvky` SET `c2` = '1158' WHERE `id` = '261';
-UPDATE `oblast_prvky` SET `c2` = '1158' WHERE `id` = '351';
-UPDATE `oblast_prvky` SET `c2` = '1158' WHERE `id` = '372';
